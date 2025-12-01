1ace63b1f9fc5cfccd9ac3135252a3c5b9f86f95
Pranav-7262 <varpepranav8@gmail.com>
2025-12-01 11:01:14 +0530

    ready for host

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,14 @@ dotenv.config();
 // console.log(process.env.MONGO_URI);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(cors());
+const PORT = process.env.PORT;
+app.use(
+  cors({
+    origin: "process.env.FRONTEND_URL", // allow requests from this origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/", urlRotes); // all url related routes
